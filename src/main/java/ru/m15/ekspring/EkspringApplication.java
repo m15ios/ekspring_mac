9cd3af98c8d6f07ccf1689ea825ac5b84acf0021
Mike Petrenko <m15mike@mail.ru>
2023-11-29 16:18:43 +0300

    first send

---
+++ src/main/java/ru/m15/ekspring/EkspringApplication.java
@@ -0,0 +1,16 @@
+package ru.m15.ekspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class EkspringApplication {
+
+    public static void main(String[] args) {
+        System.out.printf("we are here!");
+ //       SpringApplication.run(EkspringApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/ru/m15/ekspring/EkspringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.m15.ekspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
