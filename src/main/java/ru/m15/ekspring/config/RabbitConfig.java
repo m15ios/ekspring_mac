8bc3c6bd951ec3d57d2fa16eb5d59ac6904c43ee
Timur Isachenko <tisachenko@eastbanctech.com>
2024-01-12 19:10:07 +0100

    added connection factory to rabbit config

--- src/main/java/ru/m15/ekspring/config/RabbitConfig.java
+++ src/main/java/ru/m15/ekspring/config/RabbitConfig.java
@@ -13,9 +13,40 @@ public class RabbitConfig {
     // routing key
     public static final String rabbitQueue = "feed.urls.queue";
 
+    @org.springframework.beans.factory.annotation.Value("${spring.rabbitmq.host}")
+    private String rabbitHost;
+
+    @org.springframework.beans.factory.annotation.Value("${spring.rabbitmq.port}")
+    private int rabbitPort;
+
+    @org.springframework.beans.factory.annotation.Value("${spring.rabbitmq.username}")
+    private String rabbitUser;
+
+    @org.springframework.beans.factory.annotation.Value("${spring.rabbitmq.password}")
+    private String rabbitPass;
+
     @Bean
     public Queue myQueue() {
         return new Queue(RabbitConfig.rabbitQueue);
+    }
+
+
+    @Bean
+    public org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory() {
+        org.springframework.amqp.rabbit.connection.CachingConnectionFactory connectionFactory = new org.springframework.amqp.rabbit.connection.CachingConnectionFactory();
+        connectionFactory.setHost(rabbitHost);
+        connectionFactory.setPort(rabbitPort);
+        connectionFactory.setUsername(rabbitUser);
+        connectionFactory.setPassword(rabbitPass);
+//        connectionFactory.setAutomaticRecoveryEnabled(true);
+//        connectionFactory.setNetworkRecoveryInterval(10000);
+        return connectionFactory;
+    }
+
+    @Bean
+    public org.springframework.amqp.rabbit.core.RabbitTemplate rabbitTemplate() {
+        org.springframework.amqp.rabbit.core.RabbitTemplate rabbitTemplate = new org.springframework.amqp.rabbit.core.RabbitTemplate(connectionFactory());
+        return rabbitTemplate;
     }
 
     @Bean
